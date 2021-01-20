e91193d25e7c8abd1858aff602f84da4afcf934a
Brandon Lum <lumjjb@gmail.com>
2021-01-20 16:04:52 -0500

    Added list,ban,delete and CORS
    Signed-off-by: Brandon Lum <lumjjb@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Navbar from "./components/navbar.component"
+import AgentList from "./components/agent-list.component";
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="container">
+                <Navbar />
+                <br />
+                <Route path="/" exact component={AgentList} />
+                <Route path="/agents" exact component={AgentList} />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
